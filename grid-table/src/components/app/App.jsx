f6f2be97f14635a879a22511e8de822b2d4dad2f
AndeeDev <andrey14501450@gmail.com>
2020-03-08 12:28:10 +0200

    refactor(init): Set up react for current project
    Set up react, add eslint config, copy code to react component as chunck.
    Will be refactored in further commits.

---
+++ grid-table/src/components/app/App.jsx
@@ -0,0 +1,205 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <main className="page">
+      <div className="row expanded">
+        <div className="large-12 columns ">
+          <div className="top-bar cabinet">
+            <div className="large-12 column">
+              <a
+                className="float-left"
+                style={{ display: 'block;' }}
+                href="http://sumdu.edu.ua"
+                title="Сумський державний університет"
+              >
+                <img src="imgs/logotip_SSU_2_h42.png" width="35" height="42" alt="Логотип" />
+              </a>
+              <h1>Особистий кабінет</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row expanded">
+        <div id="top-notify" className="small-12 column text-center min-height-info notify">
+          <b className="alert" />
+          <b className="primary" />
+        </div>
+      </div>
+
+      <div className="row expanded">
+        <div className="small-12 columns">
+          <div id="docs-block" className="callout clearfix paddings">
+            <h5 id="title-docs" className="callout cabinet small clearfix margin-bottom">
+              ІН-71
+            </h5>
+            <div className="table-scroll">
+              <div className="table-wrapper">
+                <div className="table">
+                  <div className="thead">
+                    <div className="table-row date-row">
+                      <div className="fio">Student name</div>
+                      <div className="small-table-row">
+                        <div data-d="20.02" className="table-mark">20.02</div>
+                        <div data-d="20.02" className="table-mark">20.02</div>
+                        <div data-d="20.02" className="table-mark">20.02</div>
+                        <div data-d="20.02" className="table-mark">20.02</div>
+                        <div data-d="20.02" className="table-mark">20.02</div>
+                        <div data-d="20.02" className="table-mark">20.02</div>
+                        <div data-d="20.02" className="table-mark">20.02</div>
+                        <div data-d="20.02" className="table-mark">20.02</div>
+                        <div data-d="20.02" className="table-mark">20.02</div>
+                        <div data-d="20.02" className="table-mark">20.02</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="tbody">
+                    <div className="table-row">
+                      <div className="fio">Ваценко Андрей Владимирович</div>
+                      <div className="small-table-row">
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                      </div>
+                    </div>
+                    <div className="table-row">
+                      <div className="fio">Руденко Максим Сергеевич</div>
+                      <div className="small-table-row">
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                      </div>
+                    </div>
+                    <div className="table-row">
+                      <div className="fio">Руденко Максим Сергеевич</div>
+                      <div className="small-table-row">
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                      </div>
+                    </div>
+                    <div className="table-row">
+                      <div className="fio">Руденко Максим Сергеевич</div>
+                      <div className="small-table-row">
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                      </div>
+                    </div>
+                    <div className="table-row">
+                      <div className="fio">Руденко Максим Сергеевич</div>
+                      <div className="small-table-row">
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                      </div>
+                    </div>
+                    <div className="table-row">
+                      <div className="fio">Руденко Максим Сергеевич</div>
+                      <div className="small-table-row">
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                      </div>
+                    </div>
+                    <div className="table-row">
+                      <div className="fio">Руденко Максим Сергеевич</div>
+                      <div className="small-table-row">
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                      </div>
+                    </div>
+                    <div className="table-row">
+                      <div className="fio">Руденко Максим Сергеевич</div>
+                      <div className="small-table-row">
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                      </div>
+                    </div>
+                    <div className="table-row">
+                      <div className="fio">Руденко Максим Сергеевич</div>
+                      <div className="small-table-row">
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark"><input className="in" name="f" type="text" /></div>
+                        <div data-d="20.02" className="table-mark">2</div>
+                        <div data-d="20.02" className="table-mark">4</div>
+                        <div data-d="20.02" className="table-mark">5</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
